b826dc6d7651a5f83a88f80108f00341fa431aaa
Leonardo Medina <ext_lpmedina@falabella.cl>
2022-07-06 21:02:36 -0500

    82 mostrar modal : material

--- seccion6/flutter_componentes/lib/screens/alert_screen.dart
+++ seccion6/flutter_componentes/lib/screens/alert_screen.dart
@@ -3,11 +3,48 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void openDialog(){
-      showDialog(context: context, builder: builder)
+ /*
+ * context: si estamos trabajando en un statefull widget el context esta disponible en todo el statefull
+ * 
+ * pero en un stateless es necesario pasarlo por parametro
+ * 
+ * builder: es un código que construye un widget
+ */
+  void openDialog(BuildContext context){
+    var test = context;
+      showDialog(
+        barrierColor: const Color.fromARGB(76, 124, 15, 214),
+        barrierLabel: "TEST TEST",
+        barrierDismissible: false, // permite cerrar cuando se hace click en la sombra
+        context: context, 
+        builder: (context){ 
+          //creacion de dialog o modal
+          return AlertDialog( 
+                    elevation: 5,
+                    title: const Text(" titulo"),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(10)),
+                    //body del modal
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text('Este es el contenido de la alerta'),
+                        SizedBox(height: 10,),
+                        FlutterLogo(size: 100,)
+                      ]
+                      ),
+                      //boton modal
+                      actions: [
+                        TextButton(
+                          onPressed: ()=> Navigator.pop(context), 
+                          child: const Text("Cancelar"))
+                      ],
+                 );
+          }
+          );
   }
 
-/**
+
+ /*
  * INIT 
  */
   @override
@@ -26,7 +63,7 @@ class AlertScreen extends StatelessWidget {
               shape: const StadiumBorder(),
               elevation: 0
             ), */
-            onPressed: openDialog,
+            onPressed: ()=>openDialog(context),
             // onPressed: ()=> openDialog(),
              child: const Padding(
               //  padding: const EdgeInsets.all(8.0),
