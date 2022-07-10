21793f2766d3d8c2e61969888c4e2b10860b432c
Leonardo Medina <ext_lpmedina@falabella.cl>
2022-07-09 22:31:17 -0500

    91: widget form

--- seccion6/flutter_componentes/lib/screens/inputs_screen.dart
+++ seccion6/flutter_componentes/lib/screens/inputs_screen.dart
@@ -6,41 +6,74 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Leonardo',
+      'last_name': 'Medina',
+      'email': 'leo@gmail.com',
+      'password': '1234@Leo',
+      'role': 'Admin',
+    };
     return Scaffold(
       appBar: AppBar(title: Text('Inputs and forms')),
       // SingleChildScrollView: funciona como si se tuviera un listview para hacer scroll
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Column(
-            children: const [
-              CustomInputWidget(
-                labelText: 'Nombre',
-                hintText: 'Nombre de Usuario',
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CustomInputWidget(
-                labelText: 'Apellido',
-                hintText: 'Apellido de Usuario',
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CustomInputWidget(
-                labelText: 'Correo',
-                hintText: 'Correo de Usuario',
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CustomInputWidget(
-                  labelText: 'Contraseña',
-                  hintText: 'Contraseña de Usuario',
-                  obscureText: true),
-            ],
+          child: Form(
+            key: myFormKey,
+            //contenido formulario
+            child: Column(
+              children:  [
+                CustomInputWidget(
+                  labelText: 'Nombre',
+                  hintText: 'Nombre de Usuario',
+                ),
+                SizedBox(
+                  height: 30,
+                    ),
+                    CustomInputWidget(
+                  labelText: 'Apellido',
+                  hintText: 'Apellido de Usuario',
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomInputWidget(
+                  labelText: 'Correo',
+                  hintText: 'Correo de Usuario',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomInputWidget(
+                    labelText: 'Contraseña',
+                    hintText: 'Contraseña de Usuario',
+                    obscureText: true),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                    onPressed: (){
+                      // cuando se de click en el boton, se desactiva el teclado
+                      FocusScope.of(context).requestFocus(FocusNode());
+                      // si el formulario no es valido, no haga nada
+                      if( !myFormKey.currentState!.validate()){
+                        print("Formulario NO VALIDO");
+                        return;
+                      }
+                      //* imprimir valores del fermulario
+                      print(formValues);
+                    },
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Center(child: Text("Guardar")))
+                )
+              ],
+            ),
           ),
         ),
       ),
